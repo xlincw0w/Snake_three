75a45d6113c8e1f05783200dcca5212c0072fdbd
xlincw0w <khaled.khazem@yahoo.com>
2021-06-22 23:53:49 +0100

    preparing heroku

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
-
-// const webpack = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware');
+const PORT = process.env.PORT || 3001
 
 const app = express()
-// const config = require('./webpack.config.js');
-// const compiler = webpack(config);
 
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'dist')))
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: config.output.publicPath,
-// }));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
-
-// Serve the files on port 3000.
-app.listen(3001, function () {
-    console.log('Server listenning on port 3000!\n')
+app.listen(PORT, function () {
+    console.log('Server listenning !\n')
 })
